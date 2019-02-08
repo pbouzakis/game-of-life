561f348f75cfde64a12f0d56aa35d071a7b29017
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2019-02-08 16:15:13 -0500

    Adds fn to get live neighbors from a cell.

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,6 +1,15 @@
+import range from './range';
+
+type Generation = boolean[][];
+
 type Cell = {
   isAlive: boolean;
   neighbors: Cell[];
+};
+
+type Neighbor = {
+  location: Location,
+  isAlive: boolean,
 };
 
 const tick = (cell: Cell) => ({
@@ -32,9 +41,48 @@ const sumLiveNeighbors = (cell: Cell) => (
   ), 0)
 );
 
-const Doubles = {
+type Location = [number, number];
+
+const getLiveNeighborsFrom = ([x, y]: Location, gen: Generation) => {
+  const top = x - 1;
+  const bottom = x + 1;
+  const left = y - 1;
+  const right = y + 1;
+
+  const neighbors = [
+    getNeighbor([top, left], gen),
+    getNeighbor([top, y], gen),
+    getNeighbor([top, right], gen),
+    getNeighbor([x, left], gen),
+    getNeighbor([x, right], gen),
+    getNeighbor([bottom, left], gen),
+    getNeighbor([bottom, y], gen),
+    getNeighbor([bottom, right], gen),
+  ];
+
+  return neighbors.filter(neighbor => neighbor.isAlive);
+};
+
+const getNeighbor = ([x, y]: Location, gen: Generation): Neighbor => ({
+  location: [x, y],
+  isAlive: Boolean(gen[x] && gen[x][y]),
+});
+
+const Doubles: any = {
   toLiveCell: (neighbors: Cell[] = []): Cell => ({ isAlive: true, neighbors }),
+
   toDeadCell: (neighbors: Cell[] = []): Cell => ({ isAlive: false, neighbors }),
+
+  toDeadGeneration: (size: number) => range(size).map(() => range(size).map(() => false)),
+
+  generationBuilder: (size: number, gen: Generation = Doubles.toDeadGeneration(size)) => ({
+    addLiveCell: (x: number, y: number) => (
+      Doubles.generationBuilder(size, gen.map((row, i) => (
+        row.map((isAlive, j) => i === x && j === y ? true : isAlive)
+      )))
+    ),
+    build: () => gen,
+  }),
 };
 
 describe('On each next tick', () => {
@@ -142,6 +190,45 @@ describe('On each next tick', () => {
 
         expect(tick(cellWithOneLiveNeighbor).isAlive).toBeFalsy();
       });
+    });
+  });
+});
+
+describe('Within generation', () => {
+  describe('given a generation of size 3 with top, left, right, bottom from the center are alive', () => {
+    let gen: Generation;
+
+    beforeEach(() => {
+      gen = Doubles.generationBuilder(3)
+        .addLiveCell(0, 0) // TOP LEFT
+        .addLiveCell(0, 1) // TOP
+        .addLiveCell(1, 0) // LEFT
+        .addLiveCell(1, 2) // RIGHT
+        .addLiveCell(2, 1) // BOTTOM
+        .build();
+    });
+
+    it('finds all live neighbors from center', () => {
+      const centerCell: Location = [1, 1];
+      const neighbors = getLiveNeighborsFrom(centerCell, gen);
+
+      expect(neighbors.map(neighbor => neighbor.location)).toEqual([
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 2],
+        [2, 1],
+      ]);
+    });
+
+    it('finds all live neighbors from top left', () => {
+      const topLeft: Location = [0, 0];
+      const neighbors = getLiveNeighborsFrom(topLeft, gen);
+
+      expect(neighbors.map(neighbor => neighbor.location)).toEqual([
+        [0, 1],
+        [1, 0],
+      ]);
     });
   });
 });
