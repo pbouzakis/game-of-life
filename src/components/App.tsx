eb1c99d157fe613f442580d3256dfe53a8cc69ee
Paul Bouzakis <888851+pbouzakis@users.noreply.github.com>
2019-02-09 08:15:40 -0500

    Adds router to UI w/ start screen.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './game-of-life-logo.png';
 import './App.css';
 import Game from './Game';
+import Start from './Start';
 
 const App: React.SFC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-    </header>
-    <main>
-      <Game />
-    </main>
-  </div>
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+      </header>
+      <main>
+        <Route exact path="/" component={Start} />
+        <Route exact path="/game" component={Game} />
+      </main>
+      <footer>
+      </footer>
+    </div>
+  </Router>
 );
 
 export default App;
---
+++ src/components/Start.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Start: React.SFC = () => (
+  <section>
+    Welcome to Game Of Life
+  </section>
+);
+
+export default Start;
